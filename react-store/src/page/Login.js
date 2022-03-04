893de2b839ba0c71e3b85d9eca565ab1f50b18d5
chongyuy <chongyuy@sfu.ca>
2022-03-03 21:34:56 -0800

    new commit

--- react-store/src/page/Login.js
+++ react-store/src/page/Login.js
@@ -2,10 +2,22 @@ import React from "react";
 import "../css/app.scss";
 import "../css/style.scss";
 class Login extends React.Component {
+  handSubmit = event => {
+    //1. stop default event
+    event.preventDefault();
+    console.log('You clicked submit.');
+
+    //2. acquire the data from form
+
+    //3. process login logic
+
+    //4. jump to the app view
+    // this.props.history.push('/')
+  }
   render() {
     return (
       <div className="login-wrapper">
-        <form className="box login-box">
+        <form className="box login-box" onSubmit={this.handSubmit}>
           <div className="field">
             <label className="label">Email</label>
             <div className="control">
